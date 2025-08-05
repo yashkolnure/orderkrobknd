d1765e8a8bc2383adc46afb37cce859ab74e4d04
unknown <yashkolnure58@gmail.com>
2025-08-05 09:19:01 +0530

    Initial commit

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ console.log("✅ .env loaded");
 // Create express app
 const app = express();
 
-app.use(cors());
-
+app.use(cors());               // Allow all origins
+app.options('*', cors());      // Handle preflight requests
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
